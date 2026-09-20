b0fc9a754e7d9202d0c8fff3167a9f1f5c52886c
Victor Matia-Cheng <vmatir@gmail.com>
2026-09-20 16:07:00 +0100

    Fix the GLFW event system.

--- CanDoUrco.Glfw/GlfwEvents.cs
+++ CanDoUrco.Glfw/GlfwEvents.cs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+using CanDoUrco.Glfw.Utilities;
+
 namespace CanDoUrco.Glfw;
 
 /// <summary>
@@ -12,22 +14,41 @@ public static class GlfwEvents
     /// <summary>
     /// Processes all pending events.
     /// </summary>
-    public static void Poll() => Native.Glfw.PollEvents();
+    /// <exception cref="GlfwException">Thrown when an internal GLFW error happens.</exception>
+    public static void Poll()
+    {
+        Native.Glfw.PollEvents();
+        GlfwErrorUtilities.CheckErrorCodeAndMaybeThrowError();
+    }
 
     /// <summary>
     /// Waits until events are queued and processes them.
     /// </summary>
-    public static void WaitEvents() => Native.Glfw.WaitEvents();
+    /// <exception cref="GlfwException">Thrown when an internal GLFW error happens.</exception>
+    public static void WaitEvents()
+    {
+        Native.Glfw.WaitEvents();
+        GlfwErrorUtilities.CheckErrorCodeAndMaybeThrowError();
+    }
 
     /// <summary>
     /// Waits with timeout until events are queued and processes them.
     /// </summary>
-    /// <param name="timeout"></param>
-    public static void WaitEvents(TimeSpan timeout) =>
+    /// <param name="timeout">A <see cref="TimeSpan"/> with the time to wait for.</param>
+    /// <exception cref="GlfwException">Thrown when an internal GLFW error happens.</exception>
+    public static void WaitEvents(TimeSpan timeout)
+    {
         Native.Glfw.WaitEventsTimeout(timeout.TotalSeconds);
+        GlfwErrorUtilities.CheckErrorCodeAndMaybeThrowError();
+    }
 
     /// <summary>
     /// Posts an empty event to the event queue.
     /// </summary>
-    public static void PostEmpty() => Native.Glfw.PostEmptyEvent();
+    /// <exception cref="GlfwException">Thrown when an internal GLFW error happens.</exception>
+    public static void PostEmpty()
+    {
+        Native.Glfw.PostEmptyEvent();
+        GlfwErrorUtilities.CheckErrorCodeAndMaybeThrowError();
+    }
 }
